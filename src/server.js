e91639b20ba177fd73cf6e631e37918e20fd20fb
peemaphoj <peemaphoj@PEEMAPHOJs-MacBook-Air.local>
2022-06-09 15:37:33 +0700

    add route /healthz

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const app = express()
 
 const PORT = 3000
 
+// route to check if connection is ok.
+app.get('/healthz', (req, res) => res.status(200).json({ status: 'ok' }))
+
 app.listen(PORT, (err) => {
   if (err) console.log('Error in server setup')
   console.log('Server listening on Port', PORT)
