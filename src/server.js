ce903a805b8b50d86f4602cf04726aaabed73343
peemaphoj <peemaphoj@PEEMAPHOJs-MacBook-Air.local>
2022-06-09 15:43:05 +0700

    body-parser middleware added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 const PORT = 3000
+
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // route to check if connection is ok.
 app.get('/healthz', (req, res) => res.status(200).json({ status: 'ok' }))
