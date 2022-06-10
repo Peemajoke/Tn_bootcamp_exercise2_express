a044bc7cd47bcb4a40dd369f90799cd560e422ea
peemaphoj <peemaphoj@PEEMAPHOJs-MacBook-Air.local>
2022-06-10 10:44:51 +0700

    implement middleware

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import studentController from './controllers/student'
-// import validator from './request'
+import validator from './request'
 
 const router = express.Router()
 
 router
   .get('/students', studentController.getAllStudents)
-  .get('/students/:ID', studentController.getStudentById)
-  .post('/students', studentController.createStudent)
-  .put('/students/:ID', studentController.updateStudentById)
-  .delete('/students/:ID', studentController.deleteStudentById)
+  .get('/students/:ID', validator.getById, studentController.getStudentById)
+  .post('/students', validator.create, studentController.createStudent)
+  .put('/students/:ID', validator.updateById, studentController.updateStudentById)
+  .delete('/students/:ID', validator.deleteById, studentController.deleteStudentById)
   .get('/students/:ID/gpax', studentController.getGpaxOfStudentById)
 
 export default router
